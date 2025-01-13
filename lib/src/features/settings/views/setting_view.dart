a99634561db5bad1b2ed6b519e846a490a8a1e9f
William Franco <williamff@outlook.com>
2025-01-13 18:12:53 -0300

    :white_check_mark: Added feature settings

---
+++ lib/src/features/settings/views/setting_view.dart
@@ -0,0 +1,76 @@
+import 'package:decibel_monitor/src/common/dependency_injectors/dependency_injector.dart';
+import 'package:decibel_monitor/src/features/settings/controllers/setting_controller.dart';
+import 'package:decibel_monitor/src/features/settings/models/setting_model.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SettingView extends StatefulWidget {
+  const SettingView({super.key});
+
+  @override
+  State<SettingView> createState() => _SettingViewState();
+}
+
+class _SettingViewState extends State<SettingView> {
+  late final SettingController settingController;
+
+  @override
+  void initState() {
+    super.initState();
+    settingController = locator<SettingController>();
+  }
+
+  void _showAboutDialog() {
+    showAboutDialog(
+      context: context,
+      applicationIcon: const FlutterLogo(),
+      applicationName: 'Decibel Monitor',
+      applicationVersion: 'Version 1.0.0',
+      applicationLegalese: '\u{a9} 2024 William Franco',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('Settings'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_outlined),
+          onPressed: () {
+            context.pop();
+          },
+        ),
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: const Icon(Icons.brightness_6_outlined),
+              title: const Text('Dark theme'),
+              trailing: ValueListenableBuilder<SettingModel>(
+                valueListenable: settingController,
+                builder: (context, value, widget) {
+                  return Switch(
+                    value: value.isDarkTheme,
+                    onChanged: (bool isDarkTheme) {
+                      settingController.changeTheme(isDarkTheme: isDarkTheme);
+                    },
+                  );
+                },
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.info_outline),
+              title: const Text('About'),
+              onTap: () {
+                _showAboutDialog();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
